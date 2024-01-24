ff2771ae8261cc51474744eead99e140394ab752
Rohit Chivukula <rohitchivukula9@gmail.com>
2024-01-24 16:39:39 -0500

    add hand landmark detection

--- main.py
+++ main.py
@@ -4,65 +4,154 @@ import mediapipe as mp
 
 from google.protobuf.json_format import MessageToDict  # to convert message to dict keys
 
-# model
-hand_detection = mp.solutions.hands
-hands = hand_detection.Hands(
-    static_image_mode=False,
-    model_complexity=1,
-    min_detection_confidence=0.75,
-    min_tracking_confidence=0.75,
-    max_num_hands=2)
 
-# start live video capture from camera
-live_video_capture = cv2.VideoCapture(0)
+def hands():
+    # model
+    hand_detection = mp.solutions.hands
+    hands = hand_detection.Hands(
+        static_image_mode=False,
+        model_complexity=1,
+        min_detection_confidence=0.75,
+        min_tracking_confidence=0.75,
+        max_num_hands=2)
 
-while True:
-    # read video frame by frame; returns tuple
-    success, img = live_video_capture.read()
+    # start live video capture from camera
+    live_video_capture = cv2.VideoCapture(0)
 
-    # Flip the image(frame)
-    img = cv2.flip(img, 1)
+    while True:
+        # read video frame by frame; returns tuple
+        success, img = live_video_capture.read()
 
-    # Convert BGR image to RGB image
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Flip the image(frame)
+        img = cv2.flip(img, 1)
 
-    # Process the RGB image
-    results = hands.process(imgRGB)
+        # Convert BGR image to RGB image
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # If hands are present in image(frame)
-    if results.multi_hand_landmarks:
+        # Process the RGB image
+        results = hands.process(imgRGB)
 
-        # Both Hands are present in image(frame)
-        if len(results.multi_handedness) == 2:
-            # Display 'Both Hands' on the image
-            cv2.putText(img, 'Both Hands', (250, 50),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        0.9, (0, 255, 0), 2)
+        # If hands are present in image(frame)
+        if results.multi_hand_landmarks:
 
-        # If any hand present
-        else:
-            for i in results.multi_handedness:
+            # Both Hands are present in image(frame)
+            if len(results.multi_handedness) == 2:
+                # Display 'Both Hands' on the image
+                cv2.putText(img, 'Both Hands', (250, 50),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            0.9, (0, 255, 0), 2)
 
-                # Return whether it is Right or Left Hand
-                label = MessageToDict(i)['classification'][0]['label']
+            # If any hand present
+            else:
+                for i in results.multi_handedness:
 
-                if label == 'Left':
-                    # Display 'Left Hand' on
-                    # left side of window
-                    cv2.putText(img, label + ' Hand',
-                                (20, 50),
-                                cv2.FONT_HERSHEY_COMPLEX,
-                                0.9, (0, 255, 0), 2)
+                    # Return whether it is Right or Left Hand
+                    label = MessageToDict(i)['classification'][0]['label']
 
-                if label == 'Right':
-                    # Display 'Left Hand'
-                    # on left side of window
-                    cv2.putText(img, label + ' Hand', (460, 50),
-                                cv2.FONT_HERSHEY_COMPLEX,
-                                0.9, (0, 255, 0), 2)
+                    if label == 'Left':
+                        # Display 'Left Hand' on
+                        # left side of window
+                        cv2.putText(img, label + ' Hand',
+                                    (20, 50),
+                                    cv2.FONT_HERSHEY_COMPLEX,
+                                    0.9, (0, 255, 0), 2)
 
-    # Display Video and when 'q'
-    # is entered, destroy the window
-    cv2.imshow('Image', img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+                    if label == 'Right':
+                        # Display 'Left Hand'
+                        # on left side of window
+                        cv2.putText(img, label + ' Hand', (460, 50),
+                                    cv2.FONT_HERSHEY_COMPLEX,
+                                    0.9, (0, 255, 0), 2)
+
+        # Display Video and when 'q'
+        # is entered, destroy the window
+        cv2.imshow('Image', img)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+
+def hands_connections():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
+
+    # For static images:
+    IMAGE_FILES = []
+    with mp_hands.Hands(
+            static_image_mode=True,
+            max_num_hands=2,
+            min_detection_confidence=0.5) as hands:
+        for idx, file in enumerate(IMAGE_FILES):
+            # Read an image, flip it around y-axis for correct handedness output (see
+            # above).
+            image = cv2.flip(cv2.imread(file), 1)
+            # Convert the BGR image to RGB before processing.
+            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+            # Print handedness and draw hand landmarks on the image.
+            print('Handedness:', results.multi_handedness)
+            if not results.multi_hand_landmarks:
+                continue
+            image_height, image_width, _ = image.shape
+            annotated_image = image.copy()
+            for hand_landmarks in results.multi_hand_landmarks:
+                print('hand_landmarks:', hand_landmarks)
+                print(
+                    f'Index finger tip coordinates: (',
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+                )
+                mp_drawing.draw_landmarks(
+                    annotated_image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+            cv2.imwrite(
+                '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+            # Draw hand world landmarks.
+            if not results.multi_hand_world_landmarks:
+                continue
+            for hand_world_landmarks in results.multi_hand_world_landmarks:
+                mp_drawing.plot_landmarks(
+                    hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+
+    # For webcam input:
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
+
+
+if __name__ == '__main__':
+    hands_connections()
